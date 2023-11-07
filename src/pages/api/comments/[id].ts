e31019da25a74e64f2a73b315419b86742afe053
jindun619 <jindun619@naver.com>
2023-11-07 11:59:29 +0000

    2

--- src/pages/api/comments/[id].ts
+++ src/pages/api/comments/[id].ts
@@ -8,21 +8,21 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "PATCH") {
-    if (req.query.id) {
-      await client.comment.update({
-        where: {
-          id: +req.query.id,
-        },
-        data: {
-          content: req.body.content,
-          authorId: +req.body.authorId,
-          postId: +req.body.postId,
-        },
-      });
-      res.json({
-        message: `comment with id '${req.query.id}' has updated completely`,
-      });
-    }
+    // if (req.query.id) {
+    //   await client.comment.update({
+    //     where: {
+    //       id: +req.query.id,
+    //     },
+    //     data: {
+    //       content: req.body.content,
+    //       authorId: +req.body.authorId,
+    //       postId: +req.body.postId,
+    //     },
+    //   });
+    //   res.json({
+    //     message: `comment with id '${req.query.id}' has updated completely`,
+    //   });
+    // }
   } else if (req.method === "DELETE") {
     if (req.query.id) {
       await client.comment.delete({
