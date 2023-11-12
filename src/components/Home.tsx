9b54a2bb2fd2638dc7b7f9a264951b03a45a20ec
jindun619 <jindun619@naver.com>
2023-11-12 14:17:58 +0000

    1

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,29 @@
+import { useEffect } from "react";
+
+import axios from "axios";
+
 import HandleUsers from "./HandleUsers";
 import HandlePosts from "./HandlePosts";
 import HandleComments from "./HandleComments";
 import Login from "./Login";
 
 export default function HomePage() {
+  useEffect(() => {
+    axios
+      .get("/api/posts")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
-      <HandleUsers />
+      {/* <HandleUsers />
       <HandlePosts />
-      <HandleComments />
+      <HandleComments /> */}
       <Login />
     </>
   );
--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,28 +1,28 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import { PrismaClient } from "@prisma/client";
 
-const client = new PrismaClient();
+// const client = new PrismaClient();
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
-    const post = {
-      title: req.body.title,
-      content: req.body.content,
-      user: {
-        connect: {
-          id: req.body.userId,
-        },
-      },
-    };
-    await client.post.create({
-      data: post,
-    });
-    res.json({ message: req.body });
-  } else if (req.method === "GET") {
-    const posts = await client.post.findMany();
-    res.json(posts);
-  }
-}
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   if (req.method === "POST") {
+//     const post = {
+//       title: req.body.title,
+//       content: req.body.content,
+//       user: {
+//         connect: {
+//           id: req.body.userId,
+//         },
+//       },
+//     };
+//     await client.post.create({
+//       data: post,
+//     });
+//     res.json({ message: req.body });
+//   } else if (req.method === "GET") {
+//     const posts = await client.post.findMany();
+//     res.json(posts);
+//   }
+// }
