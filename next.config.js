b791691bd4508f293f02bd9e6267d15ad717dace
jindun619 <jindun619@naver.com>
2023-11-23 22:21:33 +0000

    1

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        prefetch: true
-    }
-}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
